24313beb80047d1f8a4d2bacfc9d44c9d7af4769
Vikil Naik <vikilnaik37@gmail.com>
2019-02-25 10:15:29 -0500

    Finished the reinfocement excercise

---
+++ excercise.py
@@ -0,0 +1,17 @@
+def draw_shape(options):
+  shape = ""
+
+  for r in range(options['rows']):
+    for c in range(options['cols']):
+      shape += options['char']
+
+    shape += "\n"
+
+  return shape
+
+
+your_code_goes_here = {'cols': 4, 'rows': 4, 'char': '*'}
+print(draw_shape(your_code_goes_here))
+
+your_code_goes_here = {'cols': 4, 'rows': 4, 'char': '0'}
+print(draw_shape(your_code_goes_here))
